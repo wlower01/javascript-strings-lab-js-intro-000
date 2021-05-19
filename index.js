fc4d58d034ab2d6600caa149662048830524cd03
rational-block-1a0e <william.lower2477@gmail.com>
2021-05-19 20:44:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,4 +9,6 @@ var topic = "space";
 var conversation = `${topic}`;
 var greeting = "Hello, everybody!";
 var specialGuest = "Neil deGrasse Tyson";
-var greetSpecialGuest = "Hello," + specialGuest + "!";
+var greetSpecialGuest = "Hello, " + specialGuest + "!";
+
+
